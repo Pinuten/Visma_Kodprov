a6dfa669f52537b5b0987fe1d42bcaae01a6d3b5
Pinuten <axel.818@gmail.com>
2024-06-11 23:47:09 +0200

    re delete .git from vismaSpcs folder

--- Visma.Tests/obj/Debug/net8.0/Visma.Tests.AssemblyInfo.cs
+++ Visma.Tests/obj/Debug/net8.0/Visma.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Visma.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d6dd6aaa2a815b5cb7925fdd58cd291ee120ed7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ad810642d34210320bdfd805af393f6d5d289f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Visma.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Visma.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
